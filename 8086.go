68a460f011685f3df03e0ec803bd834000033a86
AVCADO <russon@awty.org>
2020-04-28 09:35:45 -0500

    added layout for functions

--- 8086.go
+++ 8086.go
@@ -5,6 +5,26 @@ import (
 )
 
 
-func main() { 
+// proc8086 is the center of everything.
+// This is where we process the requests.
+func proc8086() { 
+	// TODO: process requests from receiveInstruc()
+}
 
+// receiveInstruc receives instructions.
+// this is where we receive them,
+// process them, then send them to the emulated
+// 8086 processor
+func receiveInstruc(channel <- chan string){
+
+} 
+
+// sendInstruc sends instructions to our emulated
+// 8086 processor
+func sendInstruc(channel <- chan string) {
+	
+}
+
+// The main function
+func main() {
 }
